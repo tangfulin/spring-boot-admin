4df51eec4e0545ab83ba25cc447daea26796deb0
Stephan Köninger <stephan.koeninger@codecentric.de>
2022-04-28 18:18:51 +0200

    fix: use of msw

--- spring-boot-admin-server-ui/src/main/frontend/mocks/server.js
+++ spring-boot-admin-server-ui/src/main/frontend/mocks/server.js
@@ -1,6 +1,6 @@
 import {setupServer} from 'msw/node';
 import mappingsEndpoint from "./instance/mappings/index.js";
 
-export const server = setupServer([
+export const server = setupServer(...[
   ...mappingsEndpoint
 ]);
